7106ec7d3e1a7c1009a11baf7b4b31a805d11d43
CD Cabrera <cabrera.code@gmail.com>
2023-01-14 00:50:47 -0500

    feat(cli): changelog, package, expose output (#29)
    * cli, expose changelog, package options
    * cmds, files, index, pass changelog, packagePaths
    * global, allow prop functions

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,18 +1,24 @@
+const { join } = require('path');
 const { commitChangelog } = require('../index');
 const { OPTIONS } = require('../global');
 
 describe('Commit Changelog', () => {
-  it('should parse commits to produce a CHANGELOG.md', () => {
-    const { mockClear } = mockObjectProperty(OPTIONS, { date: new Date('2022-10-01').toISOString() });
+  const { mockClear } = mockObjectProperty(OPTIONS, {
+    date: new Date('2022-10-01').toISOString(),
+    changelogPath: join(OPTIONS.contextPath, 'CHANGELOG.md'),
+    packagePath: join(OPTIONS.contextPath, 'package.json')
+  });
 
-    expect(commitChangelog()).toMatchSnapshot('Commit Changelog');
-
+  afterAll(() => {
     mockClear();
+  });
+
+  it('should parse commits to produce a CHANGELOG.md', () => {
+    expect(commitChangelog()).toMatchSnapshot('Commit Changelog');
   });
 
   it('should parse commits to produce a CHANGELOG.md with an override version', () => {
     const { mockClear } = mockObjectProperty(OPTIONS, {
-      date: new Date('2022-10-01').toISOString(),
       isOverrideVersion: true,
       overrideVersion: '15.0.0'
     });
